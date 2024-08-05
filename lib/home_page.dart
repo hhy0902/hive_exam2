7b8891cdd229b1a4b363a7bfaba29ddeeaefd349
hhy0902 <hhy09020@naver.com>
2024-08-05 18:05:27 +0900

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,107 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  var myBox = Hive.box('testBox');
+
+  TextEditingController titleController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hive Demo'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: titleController,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter title',
+                    ),
+                    onSubmitted: (String value) {
+                      myBox.add(value);
+                      titleController.clear();
+                      print("box value : ${myBox.values}");
+                      setState(() {});
+                    },
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    myBox.add(titleController.text);
+                    titleController.clear();
+                    print("box value : ${myBox.values}");
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.add),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.separated(
+                itemCount: myBox.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(myBox.getAt(index).toString()),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: const Divider(
+                      color: Colors.black,
+                      height: 1,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:hive_exam5/home_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  var box = await Hive.openBox('testBox');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
